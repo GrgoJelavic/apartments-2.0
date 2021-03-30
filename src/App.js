3a5d85b31959ba1a09f0ac4e74c4d8b66a52eb78
grgojelavic <grgo.jelavic@aspira.hr>
2021-03-30 15:28:51 +0200

    02-components, navbar, hero-I

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ import Apartment from './pages/Apartment';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Error from './pages/Error';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
-      <Home></Home>
-      <Apartments></Apartments>
-      <Apartment></Apartment>
-      <Contact></Contact>
-      <About></About>
-      <Error></Error>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/suites/' component={Apartments} />
+        <Route exact path='/suites/:slug' component={Apartment} />
+        <Route exact path='/contact/' component={Contact} />
+        <Route exact path='/about/' component={About} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
