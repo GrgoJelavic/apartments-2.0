b787d393f2cf83f8318e65c989300c9727d24985
grgojelavic <grgo.jelavic@aspira.hr>
2021-03-30 23:29:03 +0200

    03-components, hero-II, banner

--- src/pages/Apartments.js
+++ src/pages/Apartments.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import Hero from '../components/Hero/Hero';
+import Banner from '../components/Banner/Banner';
+import { Link } from 'react-router-dom';
 
-export default function Apartments() {
-  return <div>Hello from apartments </div>;
-}
+const Apartments = () => {
+  return (
+    <Hero hero='roomsHero'>
+      <Banner title='our apartments'>
+        <Link to='/' className='btn-primary'>
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
+};
+
+export default Apartments;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
+import Banner from '../components/Banner/Banner';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-  return <Hero hero='defaultHero' />;
+  return (
+    <Hero>
+      <Banner
+        title='finest apartments'
+        subtitle='comfortable apartments starting at €40'
+      >
+        <Link to='/suites' className='btn-primary'>
+          Our apartments
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
-
-Hero.defaultProps = {
-  hero: 'default',
-};
